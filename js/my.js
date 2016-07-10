935df37f111ac2037845b4be80b2ceb4e3188bb5
gaotenglong <tenglong00@sina.com>
2016-07-10 13:14:09 +0800

    修改thirdResume这个版本的时间轴线
    修改thirdResume这个版本的时间轴线

---
+++ js/my.js
@@ -0,0 +1,13 @@
+/**
+ * Created by gao on 2016/7/10.
+ */
+$(function(){
+    $().timelinr({
+        orientation: 	'vertical',
+        issuesSpeed: 	300,
+        datesSpeed: 	100,
+        arrowKeys: 		'true',
+        startAt:		3,
+        mousewheel:		'true'
+    });
+});
